d47896bebfb7a3e0aa4384c2da9fec96c0f7b0c0
Lucas Dos Santos Vellido <lucas@ampliato.com.br>
2021-02-22 09:07:48 -0300

    first commit

---
+++ src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-read',
+  templateUrl: './product-read.component.html',
+  styleUrls: ['./product-read.component.css']
+})
+export class ProductReadComponent implements OnInit {
+
+  products: Product[];
+  displayedColumns = ['id', 'name', 'price', 'action'];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.productService.read().subscribe(products => {
+      this.products = products;
+      console.log(this.products);
+    })
+  }
+
+  delete(id: string) : void {
+    console.log(id);
+    this.productService.delete(id).subscribe(() => {
+      this.productService.showMessage('Produto excluído com sucesso!');
+      this.ngOnInit();
+    },(error) => {
+      this.productService.showMessage('Deu pau!', true);
+    });
+  }
+
+}
---
+++ src/app/components/product/product.model.ts
@@ -0,0 +1,6 @@
+export interface Product {
+    // A ? siginifica que o id é opcional
+    id?: number;
+    name: string;
+    price: number;
+}
